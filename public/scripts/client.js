824fda5e059535c6b81075b694c79eea015f9197
Farid Asadpour <yani5752000@yahoo.com>
2021-07-20 22:26:23 +0000

    improvements made after the evaluation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,7 +3,7 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+//this will be used to prevent messages that damage the system
 const escape = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
@@ -28,7 +28,7 @@ const text = tweetObj.content.text;
 const creationTime = timeago.format(tweetObj["created_at"]);
 
 
-
+//creating the new article containing the new tweet
 const $html = `<article>
         <header class="newton-header">
           <div id="newton">
@@ -49,9 +49,9 @@ const $html = `<article>
         <footer id="tweeter-footer">
           <span>${creationTime}</span>
           <div id="icons">
-            <i class="fa-solid fa-flag"></i>
-            <i class="fa-solid fa-retweet"></i>
-            <i class="fa-solid fa-heart"></i>
+            <i id="icon1" class="fas fa-flag"></i>
+            <i id="icon2" class="fas fa-retweet"></i>
+            <i id="icon3" class="fas fa-heart"></i>
           </div>
           
         </footer>
@@ -63,37 +63,45 @@ const $html = `<article>
 const loadTweets = function() {
    $.ajax("/tweets", { method: 'GET' })
     .then(function (thePosts) {
-      console.log('Success: ', thePosts);
       renderTweets(thePosts);
     });
  };
 
 
 $(document).ready(()=>{
+  //hiding the error messages
   $("#is-empty").hide();
   $("#exceeds").hide();
   loadTweets();
 
   $("#form").submit(function( event ) {
     event.preventDefault();
+    //hiding the error messages
     $("#is-empty").hide();
     $("#exceeds").hide();
     const formData = $(form);
+    //give error message if data is null
     if (formData === null ) {
       $("#is-empty").slideDown();
       return;
     }
-    if (formData.serialize().length > 140) {
+    //getting rid of the "default" "text="
+    const text = formData.serialize().replace("text=", "");
+    //if more than 140 characters give error message
+    if (text.length > 140) {
       $("#exceeds").slideDown();
       return;
     }
+
     $.ajax("/tweets", {method: "POST", data: formData.serialize()})
       .then(function(){
+        //empty the container to avoid duplicate messages
         $(".tweet-container").empty();
         loadTweets();
         formData[0].reset();
     })
       .catch((err)=>{
+          //if the message is empty give error message
           $("#is-empty").slideDown();
       })
   });
